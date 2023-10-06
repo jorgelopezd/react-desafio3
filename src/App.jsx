ede58aa05facebfb78afaae454487e2e76d67a0d
jorgelopezd <jorge_lopezd@hotmail.com>
2023-10-05 22:46:43 -0300

    se sube formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 
+import { useState } from 'react'
 import './App.css'
 import { BaseColaboradores } from './components/BaseColaboradores'
 import Listado from './components/Listado'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Formulario from './components/Formulario'
 
 
 function App() {
-  const colaboradores = BaseColaboradores
+  const [colaboradores, setColaboradores] = useState([])
+
+  const agregarNuevoColaborador = (nuevoColaborador) => {
+    setColaboradores([...colaboradores, nuevoColaborador])
+  }
   return (
     <>
       <div>
+      <Formulario agregarNuevoColaborador={agregarNuevoColaborador} />
       <Listado colaboradores={colaboradores} />
       </div>
     </>
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+
+const Formulario = ({agregarNuevoColab}) =>{
+    const [nuevoColab, setNuevoColab]=useState({
+        nombre:"",
+        correo:"",
+        edad:"",
+        cargo:"",
+        telefono:"",
+    })
+
+    const {nombre,correo,edad,cargo,telefono}= nuevoColab;
+
+    const actualizarForm =(e) =>{
+        const {name,value} = e.target
+        setNuevoColab({...nuevoColab, [name]:value})
+    }
+    const agregarColab = () =>{
+        if (!nombre || !correo || !edad || !cargo || !telefono){
+            alert("por favor, no completa todos los campos")
+            return
+        }
+        agregarNuevoColab(nuevoColab)
+        setNuevoColab({
+            nombre:"",
+            correo:"",
+            edad:"",
+            cargo:"",
+            telefono:"",
+        })
+    }
+    return(
+        <div>
+            <h1>Agrega un nuevo colaborador</h1>
+            <form onSubmit={agregarColab} > 
+                <div>
+                    <label htmlFor="nombre" >Nombre: </label>
+                    <input type="text"
+                    name="nombre" 
+                    value={nombre} 
+                    onChange={actualizarForm}/> 
+                </div>
+                <div>
+                    <label htmlFor="correo">Correo: </label>
+                    <input type="email"
+                    name="correo"
+                    value={correo}
+                    onChange={actualizarForm}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="edad">Edad:</label>
+                    <input type="number"
+                    name="edad"
+                    value={edad}
+                    onChange={actualizarForm}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="cargo">Cargo:</label>
+                    <input type="text"
+                    name="cargo"
+                    value={cargo}
+                    onChange={actualizarForm}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="telefono">Telefono:</label>
+                    <input type="tel"
+                    name="telefono"
+                    value={telefono}
+                    onChange={actualizarForm}/>
+                </div>
+                <button type="submit">Agregar</button>
+            </form>
+        </div>
+    )
+}
+
+export default Formulario
